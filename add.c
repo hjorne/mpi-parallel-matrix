9c7daa0f4b62238498a4849d910cb9dbd65712e4
malcolm <lorbem@rpi.edu>
2016-04-05 17:09:10 -0400

    add matrix threaded

--- add.c
+++ add.c
@@ -3,7 +3,69 @@
 
 #include "add.h"
 
-double** add_matrix(double** orig, double** transpose)
+struct addThreadArgs
 {
-    return NULL;
+    double** orig;
+    double** transpose;
+    double** added;
+    int row_start;
+    int num_rows;
+    int row_length;
+}
+
+void* handleAdd(void * arg)
+{
+    struct addThreadArgs *args = arg;
+    int i, j;
+    for(i = args->matrix_row_start; i < args->matrix_row_start + args->num_rows; i++)
+    {
+        for(j = 0; j < args->row_length; j++)
+        {
+            args->added[i][j] = args->orig[i][j] + args->transpose[i][j];
+        }
+    }
+    pthread_exit(0);
+}
+
+double** add_matrix(int num_ranks, int matrix_size, int num_threads, double** orig, double** transpose)
+{
+    int numrows = matrix_size / num_ranks;
+    double** added = (double**) calloc(numrows, sizeof(double*));
+    int i;
+    for(i = 0; i < numrows, i++)
+    {
+        added[i] = (double*) calloc(matrix_size, sizeof(double));
+    }
+
+    pthread_t* thread_ids = malloc(sizeof(pthread_t) * (num_threads - 1));
+    int rows_per_thread = num_rows/(num_threads-1);
+    
+    int i;
+    for(i = 0; i<(num_threads - 1); i++)
+    {
+        struct addThreadArgs *args = malloc(sizeof(struct addThreadArgs));
+        args->transpose = transpose;
+        args->orig = orig;
+        args->matrix_row_start = (i+1)*rows_per_thread;
+        args->num_rows = rows_per_thread;
+        args->row_length = matrix_size;
+        int p = pthread_create(&thread_ids[i], NULL, handleAdd, (void*)args);
+    }
+    int j;
+    for(i = 0; i < rows_per_thread; i++)
+    {
+        for(j = 0; j < matrix_size; j++)
+        {
+            added[i][j] = orig[i][j];
+        }
+    }
+    for(i = 0; i < num_threads-1; i++){
+        rc = pthread_join(thread_ids[i], NULL);
+        if(rc == 0){
+            //Thread completed successfully
+        } else {
+            //Thread failed
+        }
+    }
+    return added;
 }
--- add.h
+++ add.h
@@ -1,6 +1,6 @@
 #ifndef _ADD_H_
 #define _ADD_H_
 
-double** add_matrix(double** orig, double** transpose);
+double** add_matrix(int num_ranks, int matrix_size, int num_threads, double** orig, double** transpose);
 
 #endif
