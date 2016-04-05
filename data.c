f93d8d347d41b81ed8a72f6e299c89ca7d55bd67
malcolm <lorbem@rpi.edu>
2016-04-05 17:24:32 -0400

    DATA WIPED WIP FIXING

--- data.c
+++ data.c
@@ -5,5 +5,16 @@
 
 double** transferData(int my_rank, int numranks, int matrix_size, double** orig, double** transpose)
 {
+    int numrows = matrix_size / num_ranks;
+    double** new_transpose = (double**) calloc(numrows, sizeof(double*));
+    int i;
+    for(i = 0; i < numrows, i++)
+    {
+        new_transpose[i] = (double*) calloc(matrix_size, sizeof(double));
+    }
 
+    for(i = 0; i < numranks; i++)
+    {
+        
+    }
 }
