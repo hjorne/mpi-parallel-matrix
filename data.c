beab538c6ef37234edf4ce35d7cbcfae06a9d500
malcolm <lorbem@rpi.edu>
2016-04-05 21:45:02 -0400

    sending and receiving, NEEDS TESTING

--- data.c
+++ data.c
@@ -12,9 +12,19 @@ double** transferData(int my_rank, int numranks, int matrix_size, double** orig,
     {
         new_transpose[i] = (double*) calloc(matrix_size, sizeof(double));
     }
-
+    
+    MPI_Request* send_reqs = malloc(sizeof(MPI_Request) * numranks * numrows);
+    MPI_Request* recv_reqs = malloc(sizeof(MPI_Request) * numranks * numrows);
     for(i = 0; i < numranks; i++)
     {
-        
+        if(i != my_rank)
+        {
+            int j;
+            for(j = 0; j < numrows; j++)
+            {
+                MPI_Isend(transpose[(i * numrows) + j], numrows, MPI_DOUBLE, i, j, MPI_COMM_WORLD, &send_reqs[i * numrows +j]);
+                MPI_Irecv(&(new_transpose[j][i * numrows]), numrows, MPI_DOUBLE, i, j, MPI_COMM_WORLD, &recv_reqs[i * numrows + j]);
+            }
+        }
     }
 }
