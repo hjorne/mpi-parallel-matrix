b3886b94a827b363516b4e1562f242ad6729bd94
Ezra Dowd <ezra.dowd@gmail.com>
2016-04-07 15:39:45 -0400

    Cleaned up the code a bit

--- collective.c
+++ collective.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 #include <mpi.h>
 
@@ -9,15 +10,13 @@ void collectiveFileWrite(char* file, int num_rows, int row_length, double** matr
 
     MPI_File fh;
     int rc = MPI_File_open(MPI_COMM_WORLD, file, MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
+    MPI_File_set_view( fh, 0, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL ) ;
 
     //int MPI_File_write_at_all(MPI_File fh, MPI_Offset offset, const void  *buf, int count, MPI_Datatype datatype, MPI_Status *status)
     int i;
-    MPI_Status s;
+    MPI_Status * s = malloc(sizeof(MPI_Status) * num_rows);
     for(i = 0; i < num_rows; i++){
-        int offset = rank * chunkSize + i * row_length * sizeof(double);
-        if(i == 0 && rank != 0){
-            offset += paddingBytes;
-        }
-        MPI_File_write_at_all(fh, offset, matrix[i], row_length, MPI_DOUBLE, &s);
+        int offset = rank * chunkSize + i * row_length * sizeof(double) + paddingBytes;
+        MPI_File_write_at_all(fh, offset, matrix[i], row_length, MPI_DOUBLE, &s[i]);
     }
 }
