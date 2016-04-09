8af22913a4b996ded9112e049488ce3141191d7d
malcolm <lorbem@rpi.edu>
2016-04-09 14:13:27 -0400

    calculated mayhem

--- groupio.c
+++ groupio.c
@@ -6,6 +6,11 @@
 #include "groupio.h"
 
 void groupFileWrite(char* file, int num_rows, int row_length, int rank, int ranksPerFile, int file_block_bytes, double** matrix){
+    long long int chunkSize = (num_rows * row_length * sizeof(double));
+    int end_block_padding = 0;
+    if(file_block_bytes > 0)
+        end_block_padding = chunkSize % file_block_bytes;
+
     int myrankblock = rank / ranksPerFile;
     int newrank = rank % ranksPerFile;
     
@@ -13,7 +18,7 @@ void groupFileWrite(char* file, int num_rows, int row_length, int rank, int rank
     sprintf(filename, "%s_%d",file, myrankblock);
 
     MPI_Comm filegroup;
-    MPI_Comm_Split(MPI_COMM_WORLD, myrankblock, newrank, filegroup);
+    MPI_Comm_split(MPI_COMM_WORLD, myrankblock, newrank, &filegroup);
     
     MPI_File fh;
     MPI_File_open(filegroup, filename, MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
@@ -23,7 +28,7 @@ void groupFileWrite(char* file, int num_rows, int row_length, int rank, int rank
     MPI_Status * s = malloc(sizeof(MPI_Status) * num_rows);
     for(i = 0; i < num_rows; i++){
         //Change offset calculation
-        int offset = (rank * chunkSize) + (i * row_length * sizeof(double)) + end_block_padding*rank;
+        int offset = (newrank * chunkSize) + (i * row_length * sizeof(double)) + end_block_padding*rank;
         MPI_File_write_at(fh, offset, matrix[i], row_length, MPI_DOUBLE, &s[i]);
     }
 }
